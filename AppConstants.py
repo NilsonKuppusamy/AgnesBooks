55e86aa120a6866ec7ebf1ddd9cb9ee38c8c69e0
Nilson Kuppusamy -X (nilkuppu - OBJECT FRONTIER INC at Cisco) <nilkuppu@cisco.com>
2019-04-17 18:21:23 +0530

    AgnesBooks to AgnesProds migration

--- AppConstants.py
+++ AppConstants.py
@@ -1,6 +1,6 @@
 class AppConstants :
  
 	loginCredentials = {"username": "admin", "password": "password"}
-	books = [{"name": "Ponniyin selvan", "price": 120},
-			{"name": "Selfish Giant", "price": 200}
-			]
+	products = [{"name": "iPhone SE", "description": "Apple iPhone SE smartphone was launched in March 2016. The phone comes with a 4.00-inch touchscreen display with a resolution of 640x1136 pixels at a pixel density of 326 pixels per inch (ppi).", "price": 19000, "ownerName": "admin"},
+			    {"name": "Copper Bottle", "description": "Fill the vessel or bottle with water for about 6-8 hours prior to initial consumption to allow the process of Oligodynamic to take place.", "price": 600, "ownerName": "nonadmin"}
+			   ]
--- Books.py
+++
@@ -1,8 +0,0 @@
-class Books :
-
-	def __init__(self, name, price) :
-		self.name = name
-		self.price = price
-
-	def getBooks(self) :
-		return {"name": self.name, "price": self.price}
