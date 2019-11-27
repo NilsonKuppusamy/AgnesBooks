486819a5422a3c34789ffc0995f288dd331bfdb5
Nilson Kuppusamy -X (nilkuppu - OBJECT FRONTIER INC at Cisco) <nilkuppu@cisco.com>
2019-11-27 16:13:21 +0530

    Logout logic added

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
+import json
 from Products import Products
 from AppConstants import AppConstants
 
@@ -19,15 +20,23 @@ def home():
 		return render_template('dashboard.html', products = products)
  
 @app.route('/login', methods=['POST'])
-
 def do_admin_login():
 	userCredentials = {"username": "admin", "password": "password"}
 	if request.form['password'] == userCredentials['password'] and request.form['username'] == userCredentials['username']:
 		session['logged_in'] = True
 	else:
 		flash('wrong password!')
+		return home()
+
+@app.route('/dashboard', methods=['GET'])
+def navigate_dashboard_page():
 	return home()
- 
+
+@app.route('/logout')
+def do_logout():
+	session['logged_in'] = False
+	return home()
+
 if __name__ == "__main__":
 	app.secret_key = os.urandom(12)
 	app.run(debug=True,host='192.168.11.105', port=4000)
